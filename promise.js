23ec359f3c83f42ef4e08091905e31201c01a421
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-01-12 23:20:06 +0530

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,29 @@
+const promise = new Promise((resolve, reject) => {
+
+    const data = {
+        firstName: 'Samar'
+    }
+
+    console.log(`Initial Data: ${JSON.stringify(data)}`);
+
+    setTimeout(() => {
+        data.middleName = 'Singh';
+        resolve(data);
+    }, 2000)
+
+})
+
+promise.then(data => {
+    console.log(`Data updated with middleName: ${JSON.stringify(data)}`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.lastName = 'Chauhan';
+            resolve(data);
+        }, 2000)
+    })
+
+})
+    .then(data => {
+            console.log(`Data updated with lastName: ${JSON.stringify(data)}`);
+        }
+    );
