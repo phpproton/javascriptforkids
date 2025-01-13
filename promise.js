908bc475a8b780c5869744943a927e4cdaf8136e
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-01-13 22:43:44 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -2,34 +2,30 @@
 // Run on NodeJS or Browser
 
 const promise = new Promise((resolve, reject) => {
+  const data = {
+    firstName: "Samar",
+  };
 
-    const data = {
-        firstName: 'Samar'
-    }
+  console.log(`Initial Data: ${JSON.stringify(data)}`);
 
-    console.log(`Initial Data: ${JSON.stringify(data)}`);
+  setTimeout(() => {
+    data.middleName = "Singh";
+    resolve(data);
+  }, 2000);
+});
 
-    setTimeout(() => {
-        data.middleName = 'Singh';
-        resolve(data);
-    }, 2000)
-
-})
-
-promise.then(data => {
+promise
+  .then((data) => {
     console.log(`Data updated with middleName: ${JSON.stringify(data)}`);
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.lastName = 'Chauhan';
-            resolve(data);
-        }, 2000)
-    })
+      setTimeout(() => {
+        data.lastName = "Chauhan";
+        resolve(data);
+      }, 2000);
+    });
+  })
+  .then((data) => {
+    console.log(`Data updated with lastName: ${JSON.stringify(data)}`);
+  })
 
-})
-    .then(data => {
-            console.log(`Data updated with lastName: ${JSON.stringify(data)}`);
-            return data;
-        }
-    )
-
-    .finally((data) => console.log(`Completed.`))
+  .finally(() => console.log(`COMPLETED.`));
