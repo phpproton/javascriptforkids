1fe83b23037ecd15bef1add4fad1535ccf339137
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-01-14 13:49:51 +0530

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -1,1 +1,37 @@
+// Rohit: Illustrating Prototypes in JavaScript.
+// Run on NodeJS or Browser
 
+const cat = new Object({
+  species: "cat",
+  habit: "carnivore",
+  legs: "quadraped",
+});
+
+const percy = Object.create(cat);
+
+// Prototype just means 'parent object' to which you can attach properties or functions to expand functionality
+Object.prototype.saySomething = function () {
+  console.log("Meow!");
+};
+
+percy.saySomething();
+
+// Example with a String
+const name = new String("Samar Singh Chauhan");
+
+// Custom Function
+String.prototype.makeItBig = function () {
+  return this.toUpperCase();
+};
+
+console.log(name.makeItBig());
+
+// Example with an Array
+const cars = ["Mercedes", "Maruti", "Fiat", "Hyundai", "Tata"];
+
+// Custom Function
+Array.prototype.iNeedM = function () {
+  return this.filter((car) => car.startsWith("M"));
+};
+
+console.log(cars.iNeedM());
