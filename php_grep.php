0a6525499b5bdaa2d75afff47ed2eeaddce6f7e8
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2025-03-29 00:35:33 +0530

    grep tool in PHP

--- php_grep.php
+++ php_grep.php
@@ -3,6 +3,11 @@
 
 // Rohit: Grep tool in PHP
 
+//ANSI Escape Codes
+$RED = "\e[31m";
+$RESET = "\e[0m";
+
+
 if ($argc < 3) {
     fwrite(STDERR, "Usage: {$argv[0]} <pattern> <file>\n");
     exit(1);
@@ -19,8 +24,9 @@ if (!$handle) {
 }
 
 while (($line = fgets($handle)) !== false) {
-    if (preg_match("/$pattern/", $line))
-        echo $line;
+    if (preg_match("/($pattern)/", $line, $matches)) {
+        $coloredLine = str_replace($matches[1], "$RED{$matches[1]}$RESET", $line);
+        echo $coloredLine;
+    }
 }
-
 fclose($handle);
